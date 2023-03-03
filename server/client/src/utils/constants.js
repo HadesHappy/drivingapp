308d9989c4a441643c02b1a5443721460aab67e0
HadesHappy <happyhades123@gmail.com>
2023-03-03 20:48:32 +0900

    url gi

--- server/client/src/utils/constants.js
+++ server/client/src/utils/constants.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://autoapp.es'
-// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://64.44.141.19:8080'
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'https://autoapp.es/'
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://64.44.141.19:8080/'
 // const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/'
 
 const CHEATNUM = 16
